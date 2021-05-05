493e7d08b238a70fff23e258b2c24ea013289f68
Sashika Bandara <sashikab85@gmail.com>
2021-05-05 11:44:59 +0530

    adding monitoring system files

---
+++ src/main/java/com/matific/admin/GamesMonitoringSystemApplication.java
@@ -0,0 +1,13 @@
+package com.matific.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamesMonitoringSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamesMonitoringSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/matific/admin/GamesMonitoringSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matific.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamesMonitoringSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
